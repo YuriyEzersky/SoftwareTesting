d842a54e7da33009372ae297f4e37a6d3c04327a
YuriyEzersky <faredat@gmail.com>
2024-07-11 18:19:11 +0300

    Задание №4 переход на py.test

--- NewGroups.py
+++ NewGroups.py
@@ -1,82 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from group import Group
+from application import Application
 
-class NewGroups(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
-    
-    def test_new_groups(self):
-        wd = self.open_home_page()
-        self.login(wd, "admin", "secret")
-        self.create_new_group(wd, Group("Ezerskii", "Header", "footer"))
-        self.go_home(wd)
 
-    def test_empty_groups(self):
-        wd = self.open_home_page()
-        self.login(wd, "admin", "secret")
-        self.create_empty_group(wd, Group("", "", ""))
-        self.go_home(wd)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def go_home(self, wd):
-        wd.find_element(By.LINK_TEXT, "home").click()
 
-    def create_new_group(self, wd, group):
-        wd.find_element(By.NAME, "new").click()
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        wd.find_element(By.NAME, "submit").click()
+def test_new_groups(app):
+    app.login("admin", "secret")
+    app.create_new_group(Group("Ezerskii", "Header", "footer"))
+    app.go_home()
 
-    def create_empty_group(self, wd, group):
-        wd.find_element(By.NAME, "new").click()
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        wd.find_element(By.NAME, "submit").click()
 
-    def login(self, wd, Login, Password):
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(Login)
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(Password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        return wd
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert() #устаревший метод, нужно будет рефакторить
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_empty_groups(app):
+    app.login("admin", "secret")
+    app.create_empty_group(Group("", "", ""))
+    app.go_home()
---
+++ application.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Application:
+    def __init__(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+
+
+    def login(self, Login, Password):
+        wd = self.open_home_page()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys(Login)
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys(Password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/group.php")
+        return wd
+
+    def create_new_group(self, group):
+        wd = self.open_home_page()
+        wd.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        wd.find_element(By.NAME, "submit").click()
+
+    def create_empty_group(self, group):
+        wd = self.wd
+        wd.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        wd.find_element(By.NAME, "submit").click()
+
+    def go_home(self):
+        wd = self.wd
+        wd.find_element(By.LINK_TEXT, "home").click()
+
+
+    def destroy(self):
+        self.wd.quit()
